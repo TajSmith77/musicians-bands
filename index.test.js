5005a927ae1386fc6f1d3eda697faa4fad06c05a
Taj Smith <TajSmith@Deloitte.com>
2024-03-20 16:30:17 -0400

    Added Song and Musician tests

--- index.test.js
+++ index.test.js
@@ -18,8 +18,15 @@ describe('Band, Musician, and Song Models', () => {
     })
 
     test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        //test creating a musician
+        const testMusician = await Musician.create({name: 'Josh Vietti', instrument: 'violin'})
+        expect(testMusician.name).toBe('Josh Vietti');
+    })
+
+    test('can create a Song', async () => {
+        //test creating a song
+        const testSong = await Song.create({title: 'Under Pressure', year: 2014, length: 9.2})
+        expect(testSong.title).toBe('Under Pressure');
     })
 
     test('can update a Band', async () => {
@@ -29,7 +36,18 @@ describe('Band, Musician, and Song Models', () => {
 
     test('can update a Musician', async () => {
         // TODO - test updating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        let testMusician = await Musician.create({name: 'Josh Vietti', instrument: 'violin'})
+        await Musician.update({name: 'Lindsey Stirling'}, {where: {name: 'Josh Vietti'}});
+        testMusician = await Musician.findOne({where: { instrument: 'violin'}});
+        expect(testMusician.name).toBe('Lindsey Stirling');
+    })
+
+    test('can update a Song', async () => {
+        //test updating a song
+        let testSong = await Song.create({title: 'Under Pressure', year: 2014, length: 9.2});
+        await Song.update({title: 'City of Stars', year: 2015, length:6.17 }, {where: {year: 2014}});
+        testSong = await Song.findOne({where: { year: 2015}});
+        expect(testSong.title).toBe('City of Stars');
     })
 
     test('can delete a Band', async () => {
@@ -39,6 +57,17 @@ describe('Band, Musician, and Song Models', () => {
 
     test('can delete a Musician', async () => {
         // TODO - test deleting a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const testMusician = await Musician.create({name: 'Josh Vietti', instrument: 'violin'})
+        await Musician.destroy({where: {name: 'Josh Vietti'}});
+        deletedMusician = await Musician.findOne({where: { name: 'Josh Vietti'}});
+        expect(deletedMusician).toBeNull();
+    })
+
+    test('can delete a Song', async () => {
+        //test deleting a song
+        const testSong = await Song.create({title: 'Under Pressure', year: 2014, length: 9.2});
+        await Song.destroy({where: {title: "Under Pressure"}});
+        const deletedSong = await Song.findOne({ where: { title: 'Under Pressure' } });
+        expect(deletedSong).toBeNull();
     })
 })
