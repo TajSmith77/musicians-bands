13dbe1db0860aab27573a9c98732a0aeb50332fa
Taj Smith <TajSmith@Deloitte.com>
2024-03-20 15:40:10 -0400

    Created model files

--- models/Band.js
+++ models/Band.js
@@ -1,7 +1,20 @@
+const { DataTypes } = require('sequelize');
 const {Sequelize, sequelize} = require('../db');
 
 // TODO - define the Band model
 let Band;
+
+Band = sequelize.define('Band', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+{})
 
 module.exports = {
     Band
